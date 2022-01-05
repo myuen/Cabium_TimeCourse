8dcbdb999b08926bd586ed6e87375ab7c3057b84
Mack Yuen <myuen@mail.ubc.ca>
2022-01-05 13:12:49 -0800

    Working draft. Unfinish

--- analysis/99_clustering.R
+++ analysis/99_clustering.R
@@ -1,11 +1,11 @@
 library(Mfuzz)
-library(Biobase)
 
 library(dplyr)
 library(stringr)
 library(tibble)
 library(tidyselect)
 
+<<<<<<< HEAD
 cpm <- read.table("results/tmm_normalized.cpm.txt")
 
 # cpm <- read.table("~/TPM_Pg653_Cambium_TRD_4april2017_nr.txt")
@@ -14,6 +14,15 @@ rowVar <- function(x, ...) {
   rowSums((x - rowMeans(x, ...))^2, ...)/(dim(x)[2] - 1)
 }
 
+=======
+# Function to calculate row variance
+rowVar <- function(x) {
+  rowSums((x - rowMeans(x))^2)/(dim(x)[2] - 1)
+}
+
+cpm <- read.table("results/tmm_normalized.cpm.txt")
+
+>>>>>>> 59ea49d104c283cfeb52ad68283c58f7732c97f3
 cpm.stats <-
   cpm %>% transmute(
     "MJ.T1" = rowMeans(select(cpm, starts_with("MJ.T1"))),
@@ -21,12 +30,16 @@ cpm.stats <-
     "MJ.T3" = rowMeans(select(cpm, starts_with("MJ.T3"))),
     "MJ.T4" = rowMeans(select(cpm, starts_with("MJ.T4"))),
     "MJ.T5" = rowMeans(select(cpm, starts_with("MJ.T5"))),
+<<<<<<< HEAD
 
+=======
+>>>>>>> 59ea49d104c283cfeb52ad68283c58f7732c97f3
     "TW.T1" = rowMeans(select(cpm, starts_with("TW.T1"))),
     "TW.T2" = rowMeans(select(cpm, starts_with("TW.T2"))),
     "TW.T3" = rowMeans(select(cpm, starts_with("TW.T3"))),
     "TW.T4" = rowMeans(select(cpm, starts_with("TW.T4"))),
     "TW.T5" = rowMeans(select(cpm, starts_with("TW.T5"))),
+<<<<<<< HEAD
 
     "MJ.mean" = rowMeans(select(cpm, starts_with("M"))),
     "TW.mean" = rowMeans(select(cpm, starts_with("T"))),
@@ -99,11 +112,57 @@ rownames(sigDE)[1] <- "time"
 tmp <- tempfile()
 
 write.table(sigDE, file = tmp, sep = '\t', quote = F, col.names = NA)
+=======
+    
+    # Calculate mean for treated and untreated libraries
+    "MJ.mean" = rowMeans(select(cpm, starts_with("M"))),
+    "T.mean" = rowMeans(select(cpm, starts_with("T"))),
+    
+    # Calculate variance for treated and untreated libraries
+    "MJ.var" = rowVar(select(cpm, starts_with("M"))),
+    "T.var" = rowVar(select(cpm, starts_with("T")))
+)
+
+
+# Take only the most highly express / variable
+# This is just for the sake of this tutorial
+# test_data <-
+#   cpm.stats[which(cpm.stats$MJ.var > 50 &
+#                     cpm.stats$MJ.mean > 50), c(1:5)]
+# str(test_data)
+# 'data.frame':	3997 obs. of  7 variables:
+
+mj.top <- cpm.stats %>%
+  filter(MJ.mean > 100 & MJ.var > 100) %>%
+  select(1:5)
+
+mj.top <- cpm.stats %>%
+  select(1:5)
+
+mj.top <- mj.top[!rowSums(mj.top) == 0,]
+
+str(mj.top)
+
+# all.equal(test_data, mj.top)
+# [1] TRUE
+
+
+time <- c(2, 24, 48, 96, 192)
+
+mj.top <- rbind(time, mj.top)
+
+rownames(mj.top)[1] <- "time"
+
+tmp <- tempfile()
+
+write.table(mj.top, file = tmp, sep = '\t', quote = F, col.names = NA)
+>>>>>>> 59ea49d104c283cfeb52ad68283c58f7732c97f3
 
 data <- table2eset(tmp)
 
 data.s <- standardise(data)
 
+<<<<<<< HEAD
 (m1 <- mestimate(data.s))
 # [1] 2.004545
 
@@ -133,7 +192,32 @@ dev.off()
 
 clusters <- as.data.frame(c[3])
 clusters <- clusters %>% rownames_to_column("cds")
+=======
+m1 <- mestimate(data.s)
+# [1] 2.037463
+
+Dmin(data.s, m = m1, crange = seq(2, 22, 1), repeats = 5, visu = TRUE)
+# Crashed with full set of data
+
+clust = 8
+
+c <- mfuzz(data.s, c = clust, m = m1)
+
+# mfuzz.plot(data.s, cl = c, mfrow = c(4, 3),
+#            time.labels = c(2, 24, 48, 96, 192),
+#            new.window = FALSE)
+
+>>>>>>> 59ea49d104c283cfeb52ad68283c58f7732c97f3
 
 write.table(clusters, "results/cluster_members.txt", 
             quote = FALSE, sep = "\t", row.names = FALSE)
+
+acore <- acore(data.s, c, min.acore = 0)
+
+acore_list <- 
+  do.call(rbind, lapply(seq_along(acore), function(i){
+    data.frame(CLUSTER=i, acore[[i]])}))
+
+
+### Clustering base on logFC instead of CPM (?)
 
