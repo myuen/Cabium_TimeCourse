f68c9916895f449f6992593ffcd45a496590b02a
Mack Yuen <myuen@mail.ubc.ca>
2022-01-27 14:13:09 -0800

    TODO: Add annotations to differential expression sequences.  1) Manual annotations from PG29 and 2) Transcription factors from InterProScan
    Work in progress

--- analysis/03_cluster-annotation.R
+++ analysis/03_cluster-annotation.R
@@ -1,7 +1,24 @@
 library(dplyr)
 library(stringr)
 
-clusters <- read.table("results/cluster_members.txt", 
+# Reading and merging the information from different sources.
+
+
+# Read DE statistics
+sigDE_stats <- read.delim("results/dea_sigDE_stats.txt",
+                          stringsAsFactors = FALSE)
+
+str(sigDE_stats)
+# 'data.frame':	12915 obs. of  4 variables:
+
+sigDE_up <- sigDE_stats %>% filter(logFC >= 0)
+
+str(sigDE_up)
+# 'data.frame':	7031 obs. of  4 variables:
+
+
+### Assign sigDE contigs to clusters
+clusters <- read.table("results/cluster_members.txt",
                        header = TRUE)
 
 str(clusters)
@@ -9,10 +26,88 @@ str(clusters)
 
 table(clusters$cluster)
 #    1    2    3    4    5 
-# 1963 1234 1368 1396  999 
+# 1396 1368 1234 1963  999 
+
+sigDE_up <- left_join(sigDE_up, clusters)
 
 
-ipr <- read.delim("data/sigDE.aa.annotated.tsv", 
+# Add BLAST results to annotation
+
+# Header for tab-delimited BLAST results
+blast_header <- c("qseqid", "sseqid", "evalue", "bitscore",
+                  "length", "pident", "ppos", "qcovs", 
+                  "qlen", "qstart", "qend", "qframe",
+                  "slen", "sstart", "send", "salltitles")
+
+
+# Read BLAST results from PG29 manual annotation against assembly
+annot2de <- read.delim("data/PG29ManualAnnot.blastpAssembly.txt",
+                       stringsAsFactors = FALSE, sep = "\t",
+                       header = FALSE)
+
+str(annot2de)
+# 'data.frame':	2773 obs. of  16 variables:
+
+colnames(annot2de) <- blast_header
+
+# Only select query with 95% or more identity and 80% sequence length coverage
+annot2de <- annot2de %>% filter(pident >= 95 & qcovs >= 80)
+
+str(annot2de)
+# 'data.frame':	238 obs. of  16 variables:
+
+
+# Read BLAST results from assembly BLAST against PG29 manual annotations
+de2annot <- read.delim("data/sigDE.blastpPG29ManualAnnot.txt", 
+                     stringsAsFactors = FALSE, sep = "\t",
+                     header = FALSE)
+
+colnames(de2annot) <- blast_header
+
+str(de2annot)
+# 'data.frame':	2064 obs. of  16 variables:
+
+
+# Find reciprocal best hit from BLAST results
+reciprocalBest <- 
+  inner_join(de2annot, annot2de, by = c("qseqid" = "sseqid", "sseqid" = "qseqid"))
+
+str(reciprocalBest)
+# 'data.frame':	104 obs. of  30 variables:
+
+reciprocalBest <- reciprocalBest %>% 
+  select(qseqid, sseqid)
+
+
+# Function of manual annotated genes
+geneFunc <- read.delim("data/ManualAnnotationMatrix.txt",
+                       stringsAsFactors = FALSE, header = FALSE)
+
+str(geneFunc)
+# 'data.frame':	565 obs. of  2 variables:
+
+sigDE_annotated <- 
+  left_join(reciprocalBest, geneFunc, by = c("sseqid" = "V1"))
+
+colnames(sigDE_annotated) <- c("cds", "gene", "function")
+
+str(sigDE_annotated)
+# 'data.frame':	104 obs. of  3 variables:
+
+# write.table(sigDE_annotated, "results/sigDE_manual_annotation.txt",
+            # col.names = TRUE, row.names = FALSE, 
+            # sep = "\t", quote = FALSE)
+
+
+
+
+upReg <- left_join(upReg, clusters)
+
+str(upReg)
+# 'data.frame':	7031 obs. of  5 variables:
+
+
+ipr <- read.delim("data/sigDE.aa.annotated.tsv.gz", 
                   stringsAsFactors = FALSE,
                   header = FALSE,
                   sep = "\t")
@@ -30,11 +125,14 @@ colnames(ipr) <-
     "ipr_annot", "ipr_annot_desc", "pathways")
 
 
-ipr <- ipr %>% select(cds, length, analysis, 
-                      sig_acc, sig_desc,
-                      ipr_annot, ipr_annot_desc, pathways)
+# Only keep selected columns
+ipr <- ipr %>% 
+  select(cds, length, analysis, 
+         sig_acc, sig_desc,
+         ipr_annot, ipr_annot_desc, pathways)
 
-clusters.annot <- left_join(clusters, ipr)
+
+clusters.annot <- left_join(upReg, ipr)
 
 clusters.annot$sig_desc <- str_to_upper(clusters.annot$sig_desc)
 
@@ -47,9 +145,17 @@ clusters.annot %>%
   count(cluster)
 
 #   cluster     n
-# 1       1   115
-# 2       2    75
-# 3       3    77
-# 4       4    63
-# 5       5    69
+#     <int> <int>
+# 1       1    47
+# 2       2    72
+# 3       3    60
+# 4       4    66
+# 5       5    37
 
+# Write out transcription factor CDS IDs
+tfs <- clusters.annot %>% 
+  filter(str_detect(sig_desc, "TRANSCRIPTION")) %>%
+  select(cds) %>%
+  unique()
+
+write(tfs$cds, "results/TF.cdsID.txt")
