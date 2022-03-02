971c699372aa56232b161a2031853b18391ef00d
Mack Yuen <myuen@mail.ubc.ca>
2022-03-02 14:56:38 -0800

    - Renamed variables again. - Added clone ID, InterProScan, RefSeq BLAST results to final table. - Compressed multi-line results to single line.

--- analysis/03_cluster-annotation.R
+++ analysis/03_cluster-annotation.R
@@ -3,6 +3,7 @@ library(purrr)
 library(stringr)
 library(tidyr)
 
+
 ### Add annotations to differentially expressed contigs from 3 sources:
 
 ### (1) Manually curated sequences from C. Keeling from interior Spruce PG-29
@@ -10,7 +11,7 @@ library(tidyr)
 ### (3) BLAST RefSeq Plant
 
 
-#===
+# ===
 
 
 #### Read DE statistics
@@ -35,77 +36,63 @@ table(clusters$cluster)
 # 1234  998 1368 1396 1964 
 
 
-sigDE <- left_join(sigDE_stats, clusters)
+sigDE_stats <- left_join(sigDE_stats, clusters, by = "cds")
 
 # Since we have contigs differentially expressed in multiple comparisons so we
 # have a larger count of members in respective clusters.
-table(sigDE$cluster)
+table(sigDE_stats$cluster)
 #    1    2    3    4    5 
 # 2376 1737 2634 2443 3722 
 
 
-#### Process manual curate BLAST annotations
+### 0. Add clone ID that matches contigs
+Clones <- 
+  read.delim("data/clones.megablastAssembly.txt", 
+             header = FALSE, stringsAsFactors = FALSE)
 
-# Read BLAST results from PG29 manual annotation against assembly
-blastRslt <- 
+Clones <- Clones %>% select(V2, V1)
+
+colnames(Clones) <- c("cds", "clone_id")
+
+
+
+### 1. Process manual curate BLAST annotations
+
+### Read BLAST results from PG29 manual annotation against assembly
+Manual_Annot <- 
   read.delim("data/PG29ManualAnnot.blastpAssembly.txt",
              stringsAsFactors = FALSE, sep = "\t",
              header = FALSE)
 
-#### Header for tab-delimited BLAST results
-colnames(blastRslt) <- 
-  c("qseqid", "sseqid", "evalue", "bitscore",
+# Header for tab-delimited BLAST results
+colnames(Manual_Annot) <- 
+  c("qseqid", "cds", "evalue", "bitscore",
     "length", "pident", "ppos", "qcovs", 
     "qlen", "qstart", "qend", "qframe",
-    "slen", "sstart", "send", "salltitles")
+    "slen", "sstart", "send", "manual_annot")
 
-str(blastRslt)
+str(Manual_Annot)
 # 'data.frame':	2773 obs. of  16 variables:
 
 
 # Only select query (i.e.contigs) with 95% or more identity and 80% to subject
 # sequence (i.e. manual annotated sequences) length coverage and up-regulated
-sigDE_manual_Annot <- blastRslt %>% 
-  filter(sseqid %in% sigDE$cds) %>% 
+Manual_Annot <- Manual_Annot %>% 
+  filter(cds %in% sigDE_stats$cds) %>% 
   filter(pident >= 95 & qcovs >= 80) %>% 
-  select(sseqid, qseqid)
+  select(cds, manual_annot)
 
-str(sigDE_manual_Annot)
+str(Manual_Annot)
  # 'data.frame':	109 obs. of  2 variables:
 
 
-# Add gene function to manual annotation
-manualAnnotGeneFunc <- 
-  read.delim("data/ManualAnnotationMatrix.txt",
-             stringsAsFactors = FALSE, header = FALSE)
-
-str(manualAnnotGeneFunc)
-# 'data.frame':	565 obs. of  2 variables:
-
-sigDE_manual_Annot <- 
-  left_join(sigDE_manual_Annot, manualAnnotGeneFunc, 
-            by = c("qseqid" = "V1"))
-
-sigDE_manual_Annot <- sigDE_manual_Annot %>% 
-  select(-qseqid) %>% 
-  unique()
-
-colnames(sigDE_manual_Annot) <- c("cds", "manual annotation")
-
-# sigDE_manual_Annot$annot_method <- "manual"
-
-str(sigDE_manual_Annot)
-# 'data.frame':	99 obs. of  3 variables:
-
-
-
-#### Process tsv InterProScan annotation
-ipr <- read.delim("data/sigDE.aa.annotated.tsv.gz", 
+#### 2. Process tsv InterProScan annotation
+IPR <- read.delim("data/sigDE.aa.annotated.tsv.gz", 
                   stringsAsFactors = FALSE,
                   header = FALSE,
                   sep = "\t")
 
-colnames(ipr) <- 
+colnames(IPR) <- 
   c("cds", "MD5", "length", "analysis", 
     "sig_acc", "sig_desc",
     "start_loc", "stop_loc",
@@ -114,84 +101,105 @@ colnames(ipr) <-
     "date",
     "ipr_annot", "ipr_annot_desc", "pathways")
 
-str(ipr)
+str(IPR)
 # 'data.frame':	56154 obs. of  14 variables:
 
 # Cast score as a numeric
-ipr$score <- as.numeric(ipr$score)
+IPR$score <- as.numeric(IPR$score)
 
 
 # Subset InterProScan annotation 
 # Remove NA from coercion.
+# Remove MobiDBLite annotations.  It does not add any value to our purpose.
 # Retain only those with a e-value 1e-20 or below
-ipr <- ipr %>% 
+IPR_Filtered <- IPR %>% 
   filter(!is.na(score)) %>% 
   filter(score <= 1e-20) %>% 
-  # filter()
-  select(cds, ipr_annot, ipr_annot_desc, sig_desc)
+  filter(analysis != "MobiDBLite") %>% 
+  select(cds, sig_acc, sig_desc, ipr_annot, ipr_annot_desc, sig_desc)
 
 
 # Number of counts of contigs with InterProScan annotations
-length(sigDE_IPR_Annot$cds)
-# [1] 56154
+length(IPR_Filtered$cds)
+# [1] 29924
 
 # Number of counts of unique contigs with InterProScan annotations.  What is
 # shown is there are a lot of contigs with multiple InterProScan match.
-length(unique(sigDE_IPR_Annot$cds))
-# [1] 6537
+length(unique(IPR_Filtered$cds))
+# [1] 5574
 
 
 # Compress multi-line match
-sigDE_IPR_Annot <- sigDE_IPR_Annot %>%
+IPR_Filtered <- IPR_Filtered %>%
   group_by(cds) %>% 
   nest()
 
-sigDE_IPR_Annot$data <- 
-  map_df(sigDE_IPR_Annot$data, function(n){
-
-  annot_ids <- unlist(n["annot_id"])
-  annot_ids <- annot_ids[annot_ids != "-"]
-
-  ipr_annot_descs <- unlist(n["ipr_annot_desc"])
-  ipr_annot_descs <- ipr_annot_descs[ipr_annot_descs != "-"]
+IPR_Filtered$data <- 
+  map_df(IPR_Filtered$data, function(n){
+    
+    sig_accs <- unlist(n["sig_acc"])
+    
+    # Remove cells with no info
+    sig_descs <- unlist(n["sig_desc"])
+    sig_descs <- sig_descs[sig_descs != "-"]
   
-  sig_descs <- unlist(n["sig_desc"])
-  sig_descs <- sig_descs[sig_descs != "-"]
+    ipr_annots <- unlist(n["ipr_annot"])
+    ipr_annots <- ipr_annots[ipr_annots != "-"]
 
-  
-  df <- data.frame(
-    annot_ids = str_c(annot_ids, collapse = ";"),
-    ipr_annot_descs = str_c(ipr_annot_descs, collapse = ";"),
-    sig_descs = str_c(sig_descs, collapse = ";"))
+    ipr_annot_descs <- unlist(n["ipr_annot_desc"])
+    ipr_annot_descs <- ipr_annot_descs[ipr_annot_descs != "-"]
+    
+    # Collapse multi-line annotation into single line
+    df <- data.frame(
+      sig_accs <- str_c(sig_accs, collapse = ";"),
+      
+      sig_descs <- str_c(sig_descs, collapse = ";"),
+      
+      ipr_annots <- str_c(ipr_annots, collapse = ";"),
+
+      ipr_annot_descs <- str_c(ipr_annot_descs, collapse = ";"))
 })
 
-sigDE_IPR_Annot <- sigDE_IPR_Annot %>% unnest(cols = c(data))
+IPR_Filtered <- IPR_Filtered %>% unnest(cols = c(data))
 
-sigDE_IPR_Annot$annot_method <- "InterProScan"
+colnames(IPR_Filtered) <- c("cds", "ipr_sig_accs", "ipr_sig_descs",
+                            "ipr_annots", "ipr_annot_descs")
 
-###
-
-
-# all_annots <- rbind(sigDE_manual_Annot, sigDE_IPR_Annot)
-
-# str(all_annots)
-# 'data.frame':	441 obs. of  4 variables:
+str(IPR_Filtered)
+# grouped_df [5,574 × 5] (S3: grouped_df/tbl_df/tbl/data.frame)
 
 
-# Add manual annotation to DE contigs
-sigDE_annot <- left_join(sigDE, sigDE_manual_Annot)
-# Joining, by = "cds"
 
-str(sigDE_annot)
-# 'data.frame':	12956 obs. of  7 variables:
+#### 3. Process BLAST tsv annotations
+RefSeq_Annot <- 
+  read.delim("data/sigDE.pep.blastpRefSeqPlant208.txt",
+             stringsAsFactors = FALSE, sep = "\t",
+             header = FALSE)
+
+#### Header for tab-delimited BLAST results
+colnames(RefSeq_Annot) <- 
+  c("cds", "BLAST_hit", "evalue", "bitscore", "length", "pident", "ppos", 
+    "qcovs", "qlen", "qstart", "qend", "qframe", 
+    "slen", "sstart", "send", "sframe", 
+    "staxid", "sskingdom", "sscinames", "scomnames", "BLAST_desc")
+
+RefSeq_Annot <- RefSeq_Annot %>% 
+  select(cds, BLAST_hit, BLAST_desc)
+
+str(RefSeq_Annot)
+# 'data.frame':	5773 obs. of  3 variables:
 
 
-# For contigs without manual annotation, we will add InterProScan functional
-# annotation
+#### 4. Append all results into a final table
+sigDE_Annot <- left_join(sigDE_stats, Clones, by = "cds")
+sigDE_Annot <- left_join(sigDE_Annot, Manual_Annot, by = "cds")
+sigDE_Annot <- left_join(sigDE_Annot, IPR_Filtered, by = "cds")
+sigDE_Annot <- left_join(sigDE_Annot, RefSeq_Annot, by = "cds")
 
-sigDE_annot <- sigDE_annot[order(sigDE_annot$cluster),]
+str(sigDE_Annot)
+# 'data.frame':	13676 obs. of  12 variables:
 
-write.table(sigDE_up_annot, "results/sigDE_up_annotation.txt",
+
+write.table(sigDE_Annot, "results/sigDE_annotations.txt",
             quote = FALSE, col.names = TRUE, row.names = FALSE,
             sep = "\t")
-
