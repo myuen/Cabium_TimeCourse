9f343d0f5e7e377c666a8a68e8a21430f1266505
mack <yuen.mack@gmail.com>
2023-04-03 15:46:03 -0700

    Major revision

--- analysis/02_fuzzy-clustering.R
+++ analysis/02_fuzzy-clustering.R
@@ -1,11 +1,7 @@
 library(Mfuzz)
 
 library(dplyr)
-library(ggplot2)
-library(stringr)
 library(tibble)
-library(tidyr)
-library(tidyselect)
 
 
 # Read normalized CPM
@@ -69,12 +65,12 @@ rownames(sigDE.stats)[1] <- "time"
 
 
 # table2eset function must read from a file
-write.table(sigDE.stats, file = "results/sigDE.stats.tbl.new.txt",
+write.table(sigDE.stats, file = "results/sigDE.stats.tbl.txt",
             sep = '\t', quote = F, col.names = NA)
 
 
 # Convert an expression table to eset object
-data <- table2eset("results/sigDE.stats.tbl.new.txt")
+data <- table2eset("results/sigDE.stats.tbl.feb17.txt")
 
 
 # Standardise data
@@ -82,156 +78,50 @@ data.s <- standardise(data)
 
 
 (m1 <- mestimate(data.s))
-# [1] 2.004545
+# [1] 2.005024
 
-# pdf("results/figures/dmin.pdf", width = 20, height = 10)
+
+pdf("results/figures/dmin.pdf", width = 20, height = 10)
 
 Dmin(data.s, m = m1, crange = seq(2, 10, 1), repeats = 5, visu = TRUE)
-# [1] 2.2942950 2.3378309 2.0339764 1.1626455 1.1139902 1.0279514 0.8745872 0.7924840 0.6685793
+# [1] 2.2974304 2.3323899 2.0215294 1.1607365 1.1254128 1.0166034 0.7747944
+# [8] 0.8162585 0.7447335
 
 dev.off()
 
 
+# Number of soft clusters
 clust <- 5
 
 c <- mfuzz(data.s, c = clust, m = m1)
 
-# pdf("results/figures/dea-clusters.new.pdf", width = 20, height = 10)
-# 
-# mfuzz.plot2(data.s, cl = c, mfrow = c(2, 3), colo = "fancy",
-#             time.labels = c(2, 24, 48, 96, 192),
-#             xlab = "Time (hrs)",
-#             ylab = expression("Log"[2]*'FC'),
-#             centre = TRUE,
-#             # Increase font size of title
-#             cex.main = 1.2,
-#             # Increase font size of tick point
-#             cex.axis = 1.1,
-#             # Increase font size of axis lable
-#             cex.lab = 1.2,
-#             x11 = FALSE)
-# 
-# dev.off()
+###
+
+clusters <- c[[1]]
+clusterindex <- c[[3]]
+memship <- c[[4]]
+memship[memship < 0.7] <- -1
+colorindex <- integer(dim(exprs(data.s))[[1]])
+
+x <- matrix(rnorm(5*5),nrow=5) # new expression matrix with two genes
+mem.tmp <- membership(x, clusters=clusters,m=ml) #membership values
+
+###
 
 
-#####
-
-### Standardize expression data
+# Write out standardized expression data
 s.data <- exprs(data.s) %>% 
   as.data.frame() %>% 
   rownames_to_column('cds')
 
-
-# Transform table into long format and prepare for plot
-s.data <- s.data %>% 
-  pivot_longer(2:6, names_to = "library", values_to = "expression")
-
-# extract timepoint from library name
-s.data$timepoint <- str_extract(s.data$library, "\\d$")
-
-s.data$timepoint <- as.integer(s.data$timepoint)
-
-# Convert timepoint to 'hours'
-s.data[s.data$timepoint == 5, 4] <- 192
-s.data[s.data$timepoint == 4, 4] <- 96
-s.data[s.data$timepoint == 3, 4] <- 48
-s.data[s.data$timepoint == 2, 4] <- 24
-s.data[s.data$timepoint == 1, 4] <- 2
-
-
+write.table(s.data, "results/standardized_expression.txt", 
+            quote = FALSE, row.names = FALSE)
+            
 # Write out cluster members
 clstr_members <- as.data.frame(c[3])
 
 clstr_members <- clstr_members %>% 
   rownames_to_column("cds")
 
-# write.table(clstr_members, "results/cluster_members.txt", 
-#             quote = FALSE, sep = "\t", row.names = FALSE)
-
-write.table(clstr_members, "results/cluster_members.new.txt", 
-            quote = FALSE, sep = "\t", row.names = FALSE)
-
-##### 
-
-clstr_members_exprs <- left_join(s.data, clstr_members, by = 'cds')
-
-#####
-
-bzip <- read.csv("data/TRAPID_TF_BZIP_FL.csv", stringsAsFactors = FALSE)
-bzip <- bzip %>%
-  select(2) %>%
-  add_column('tf' = 'bzip')
-
-hsf <- read.csv("data/TRAPID_TF_HSF_FL.csv", stringsAsFactors = FALSE)
-hsf <- hsf %>%
-  select(2) %>%
-  add_column('tf' = 'hsf')
-
-mads <- read.csv("data/TRAPID_TF_MADS_FL.csv", stringsAsFactors = FALSE)
-mads <- mads %>%
-  select(2) %>%
-  add_column('tf' = 'mads')
-
-nac <- read.csv("data/TRAPID_TF_NAC_FL.csv", stringsAsFactors = FALSE)
-nac <- nac %>%
-  select(2) %>%
-  add_column('tf' = 'nac')
-
-
-tf <- bind_rows(bzip, hsf, mads, nac)
-colnames(tf)[1] <- 'cds'
-
-clstr_members_exprs2 <- left_join(clstr_members_exprs, tf, by = "cds")
-
-clstr_members_exprs2$tf <- factor(clstr_members_exprs2$tf, 
-                                  levels = c('bzip', 'hsf', 'mads', 'nac'))
-
-#####
-
-jpeg("results/figures/draft.jpg", width = 3000, height = 1600, units = 'px')
-# pdf("results/figures/draft.pdf", width = 20, height = 10)
-
-# clstr_members_exprs2$tf <- replace_na(clstr_members_exprs2$tf, "na")
-clstr_members_exprs2 <- clstr_members_exprs2[order(clstr_members_exprs2$tf),]
-
-# clstr_members_exprs2$tf <- replace_na(clstr_members_exprs2$tf, "na")
-
-
-g <-
-  ggplot(clstr_members_exprs2,
-       aes(x = timepoint, y = expression, group = rev(cds), color = tf)) + 
-  geom_line() +
-  facet_wrap( ~ cluster) + 
-  
-  scale_color_manual(na.value = 'grey90', values = c('bzip' = 'blue',
-                                'hsf' = 'coral',
-                                'mads' = 'olivedrab',
-                                'nac' = 'red')) +
-    
-  # scale_linewidth_manual(values = c('bzip' = 1,
-  #                                   'hsf' = 1,
-  #                                   'mads' = 1,
-  #                                   'nac' = 1,
-  #                                   'na' = 0.5)) +
-
-  scale_alpha_manual(na.value = 0.1, values = c('bzip' = 1,
-                                'hsf' = 1,
-                                'mads' = 1,
-                                'nac' = 1)) + 
-
-  scale_x_continuous(labels = c(2, 24, 48, 96, 192),
-                     limits = c(2, 192),
-                     breaks = c(2, 24, 48, 96, 192)) +
-  
-  xlab("Time Point (Hours)") + 
-  ylab("Expression") +
-  ggtitle("Soft Clustering of differential expression contigs") +
-  
-  theme(panel.background = element_rect(fill = 'white', colour = 'grey50'))
-
-ggsave("results/figures/draft.jpg", g, width = 3000, height = 1500, units = 'px')
-
-# last_plot() + aes(group=rev(cds))
-
-dev.off()
-
+write.table(clstr_members, "results/cluster_members.feb17.txt", 
+            quote = FALSE, row.names = FALSE)
